f01b6450d001b8f184ab92304f621533ba43a3b2
Gabriel Fazenda <Gabriel.l.fazenda@gmail.com>
2018-03-23 21:00:02 -0300

    working pt2

--- Javascript/Points.js
+++ Javascript/Points.js
@@ -5,7 +5,7 @@ ctx = canvas.getContext("2d");
 var canvas = document.getElementById("DividedPointCanvas"),
 ctx2 = canvas.getContext("2d");
 
-var Points = [];
+var PointsX = [], PointsY = [], Points = [];
 var size = 16;
 var height = 500, width = 500
 var list = []
@@ -17,14 +17,17 @@ function DoTest(){
     InsertSort(Points);
     console.log(Points)
     var resultDivide = closestDivideAndConquer(Points)
+    console.log('tttt')
+    console.log(thing);
     var resultBrute = BruteForce(Points,size);
     DrawPoints()
     DrawPoints2()
     console.log('resultBrute: ' + dist(resultBrute[0],resultBrute[1]))
     console.log('resultDivide: ' + resultDivide)
     console.log(resultBrute)
-   DrawPoint(thing[0].x,thing[0].y, "#FF0000")
-   DrawPoint(thing[1].x,thing[1].y, "#FF0000")
+   
+//     DrawPoint(thing[0].x,thing[0].y, "#FF0000")
+//    DrawPoint(thing[1].x,thing[1].y, "#FF0000")
 
 }
 
@@ -65,6 +68,8 @@ function BruteForce(P, n){
          return closest;
 }
 
+
+
 function dist(p1,p2){
     return Math.sqrt((p1.x - p2.x)*(p1.x - p2.x) +
                     (p1.y - p2.y)*(p1.y - p2.y));
@@ -100,13 +105,12 @@ function closestDivideAndConquer(P){
         if(P.length==1)
             return Infinity
         if(P.length==2){
-            list.push(P[0])
-            list.push(P[1])
             return dist(P[0],P[1]);
         }
 
-        var mid = (P.length+1)/2;
+        var mid = (P.length)/2;
         console.log(mid)
+        var midPoint = P[mid]
         //var midPoint = P[mid];
         var P1 = [], P2 = [];
         for (var i = 0; i < P.length; i++) {
@@ -115,76 +119,32 @@ function closestDivideAndConquer(P){
             else
                 P2.push(P[i]);
         }
-        console.log('l1:'+P1.length)
-        console.log('l2:'+P2.length)
+        console.log('L1: '+P1.length)
+        console.log('L2: '+P2.length)
         d1 = closestDivideAndConquer(P1);
         d2 = closestDivideAndConquer(P2);
-        d3 = dist(P1[P1.length-1],P2[0]); 
-        list.push(P1[P1.length-1])
-        list.push(P2[0])
 
-        var d = Math.min(d1,d2,d3)
-        var result = d
-        // var result = d2 < d1 ? d2 : d1;
-        // result = result < d3 ? result : d3;
-        thing = []
-        if(result==d1){
-            thing.push(list[0])
-            thing.push(list[1])
+        var strip = []
+        for (var i = 0; i < P.length; i++){
+            if (Math.abs(P[i].x - midPoint.x) < d)
+                strip.push(P[i])
         }
-        else if (result ==d2){
-            thing.push(list[2])
-            thing.push(list[3])
-        }
-        else{
-            thing.push(list[4])
-            thing.push(list[5])
-        }
-        console.log(thing)
-        return d
+        var d = Math.min(d1,d2)
+        // var result = d
+        console.log('oowowo')
+        return Math.min(d,CheckClosest(strip,d))
+    }
 
-
-
-
-
-
-
-    // // If there are 2 or 3 points, then use brute force
-    // if (n <= 3)
-    //     return bruteForce(P, n);
-
-    // // Find the middle point
-    // var mid = n/2;
-    // var midPoint = P;
-
-    // // Consider the vertical line passing through the middle point
-    // // calculate the smallest distance dl on left of middle point and
-    // // dr on right side
-    // var dl = closestUtil(P, mid);
-    // var dr = closestUtil(P + mid, n-mid);
-
-    // // Find the smaller of two distances
-    // var d = min(dl, dr);
-
-    // // Build an array strip[] that contains points close (closer than d)
-    // // to the line passing through the middle point
-    // var strip = [];
-    // var j = 0;
-    // for (var i = 0; i < n; i++)
-    // if (Math.abs(P[i].x - midPoint.x) < d)
-    //     strip[j] = P[i], j++;
-
-    // // Find the closest points in strip.  Return the minimum of d and closest
-    // // distance is strip[]
-    // return min(d, stripClosest(strip, j, d) );
-}
-
-function closest(P, n)
-{
-    qsort(P, n, sizeof(Point), compareX);
- 
-    // Use recursive function closestUtil() to find the smallest distance
-    return closestUtil(P, n);
+function CheckClosest(points,minDist){
+    InsertSort(points,false)
+    var min = minDist
+    for (var i = 0; i < points.length; i++) {
+        for (var j = 0; j < points.length && (points[j].y-points[y].y)<min; j++) {
+            if (dist(points[i],points[j]) < min)
+                min = dist(points[i], points[j]);
+           }
+    }
+    return min;
 }
 
 function InsertSort(array, compareX = true){
@@ -196,12 +156,21 @@ function InsertSort(array, compareX = true){
 
 		/* Move elements of arr[0..i-1], that are
 		greater than key, to one position ahead
-		of their current position */
+        of their current position */
+        if(compareX){
 		while (j >= 0 && array[j].x > key.x)
 		{
 			array[j + 1] = array[j];
 			j = j - 1;
-		}
+        }
+    }
+        else{
+            while (j >= 0 && array[j].y > key.y)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+        }
 		array[j + 1] = key;
 	}
     }
