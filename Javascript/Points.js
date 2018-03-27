fe89781c04c1d39a6fe90d3d6c08b339ac8333d6
gfazenda <gabriel.l.fazenda@gmail.com>
2018-03-27 01:21:41 -0300

    pt3

--- Javascript/Points.js
+++ Javascript/Points.js
@@ -2,67 +2,89 @@
 var canvas = document.getElementById("BrutePointCanvas"),
 ctx = canvas.getContext("2d");
 
-var canvas = document.getElementById("DividedPointCanvas"),
-ctx2 = canvas.getContext("2d");
+var canvas2 = document.getElementById("DividedPointCanvas"),
+ctx2 = canvas2.getContext("2d");
 
 var PointsX = [], PointsY = [], Points = [];
 var size;
 var height = 500, width = 500
-var list = []
-var thing = [], thing2 = []
+var pointsToCheck = []
 
-function DoTest(){
-  
+function Predefined(){
     FillList();
-    // FillRandom(50);
+    CalculateClosestPoint()
+}
 
+function Random(){
+    FillRandom(20);
+    CalculateClosestPoint()
+}
+
+function CalculateClosestPoint(){
     InsertSort(Points);
     console.log(Points)
 
     var resultDivide = closestDivideAndConquer(Points)
 
-    var resultBrute = BruteForce(Points,size);
+    var resultBrute = BruteForce(Points);
 
-    DrawPoints()
-    DrawPoints2()
+    DrawOnCanvas()
 
     console.log('resultBrute: ' + dist(resultBrute[0],resultBrute[1]))
     console.log('resultDivide: ' + resultDivide)
-
-   
-    DrawPoint(thing[0].x,thing[0].y, "#FF0000")
-    DrawPoint(thing[1].x,thing[1].y, "#FF0000")
+    console.log('to see ' + pointsToCheck.length)
+    var closestPoints = GetPoints(resultDivide)
+    DrawPoint(closestPoints[0].x,closestPoints[0].y, "#FF0000")
+    DrawPoint(closestPoints[1].x,closestPoints[1].y, "#FF0000")
 
     DrawPoint2(resultBrute[0].x,resultBrute[0].y, "#FF0000")
     DrawPoint2(resultBrute[1].x,resultBrute[1].y, "#FF0000")
+}
 
+function GetPoints(result){
+    for (var i = 0; i < pointsToCheck.length-1; i++) {
+        for (var j = i+1; j < pointsToCheck.length ; j++) {
+            if (dist(pointsToCheck[i],pointsToCheck[j]) == result)
+                return [pointsToCheck[i],pointsToCheck[j]]
+        }
+    }
+}
+
+function DrawOnCanvas(){
+    clearCanvas(ctx,canvas)
+    clearCanvas(ctx2,canvas2)
+    DrawPoints()
+    DrawPoints2()
 }
 
 function FillList(){
-    Points[0] = {x:-13,y:0.5}
-    Points[1] = {x:-10.5,y:-11.5}
-    Points[2] = {x:-10,y:9}
-    Points[3] = {x:-4.5,y:-2}
-    Points[4] = {x:-1,y:8.5}
-    Points[5] = {x:0.5,y:6}
-    Points[6] = {x:0.5,y:-12}
-    Points[7] = {x:2,y:12.5}
-    Points[8] = {x:3.5,y:11}
-    Points[9] = {x:5.5,y:3}
-    Points[10] = {x:5.5,y:-7}
-    Points[11] = {x:5,y:11.5}
-    Points[12] = {x:6.5,y:3.2}
-    Points[13] = {x:7,y:-10}
-    Points[14] = {x:9,y:-5}
-    Points[15] = {x:11.5,y:-4} 
+    Points = []
+    Points = [{x:-13,y:0.5},{x:-10.5,y:-11.5},{x:-10,y:9},{x:-4.5,y:-2},
+    {x:-1,y:8.5},{x:0.5,y:6},{x:0.5,y:-12},{x:2,y:12.5},{x:3.5,y:11},
+    {x:5.5,y:3},{x:5.5,y:-7},{x:5,y:11.5},{x:6.5,y:3.2},{x:7,y:-10},
+    {x:9,y:-5},{x:11.5,y:-4}];
 
-    // Points = [{ x:2, y: 3}, { x:12, y: 30}, { x:40, y: 50}, { x:5, y: 1}, { x:12, y: 10}, { x:3, y: 4}];
+    // Points = [{x:2,y:1},{x:3,y:5},{x:8,y:3},{x:5,y:8},{x:9,y:1},{x:5,y:2},{x:3,y:3},{x:4,y:5},{x:6,y:5},{x:1,y:9}];
+
+    Points = [{x:2,y:1},
+        {x:8,y:3},
+        {x:5,y:8},
+        {x:9,y:1},
+        {x:5,y:2},
+        {x:3,y:3},
+        {x:4,y:5},
+        {x:6,y:5},
+        {x:1,y:9},
+        {x:2,y:1.5}];
+    //Points = [{ x:2, y: 3}, { x:12, y: 30}, { x:40, y: 50}, { x:5, y: 1}, { x:12, y: 10}, { x:3, y: 4}];
      size = Points.length
 }
 
 function FillRandom(x){
+    Points = []
+    var min = -15, max = 15
     for (var i = 0; i < x; i++) {
-        Points.push({x: GetRandom(-20,20),y: GetRandom(-20,20)})
+        Points.push({x: GetRandom(min, max),y: GetRandom(min,max)})
     }
     size = Points.length
 }
@@ -71,9 +93,10 @@ function GetRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-function BruteForce(P, n){
+function BruteForce(P){
         var minDist = Infinity;
         var closest = [];
+        var n = P.length;
         for (var i = 0; i < n; i++) {
          for (var j = i+1; j < n; j++) {
             var p = P[i], q = P[j];
@@ -119,61 +142,76 @@ function DrawPoint2(x,y, color){
     ctx2.fillRect(mW+(x*multiplier)-(pointSize/2),mH+(y*multiplier)-(pointSize/2),pointSize,pointSize);
 }
 
-
-function closestDivideAndConquer(P){
-    // console.log('a')    
-    
-        if(P.length==1)
+function closestDivideAndConquer(P){ 
+    pointsToCheck = []
+        if(P.length==1){
             return Infinity
+        }
         if(P.length==2){
+            pointsToCheck.push(P[0])
+            pointsToCheck.push(P[1])
             return dist(P[0],P[1]);
         }
-
+        
         var mid = Math.round((P.length)/2);
-        console.log(mid)
+        // console.log(mid)
         var midPoint = P[mid]
         //var midPoint = P[mid];
         var P1 = [], P2 = [];
         for (var i = 0; i < P.length; i++) {
-            if(i<mid)
+            if(i<mid){
                 P1.push(P[i]);
-            else
+            }
+            else{
                 P2.push(P[i]);
+            }
         }
+
         console.log('L1: '+P1.length)
         console.log('L2: '+P2.length)
         d1 = closestDivideAndConquer(P1);
         d2 = closestDivideAndConquer(P2);
-        console.log(midPoint)
-        console.log(P2)
-        d3 = dist(P1[mid-1], P2[0])
-        var d = Math.min(d1,d2,d3)
+       
+        console.log('d1 ' + d1)
+        
+        console.log('d2 ' + d2)
 
-        var strip = []
+        var d = Math.min(d1,d2)
+        console.log('d ' + d)
+
+        var borderPoints = []
         for (var i = 0; i < P.length; i++){
-            if (Math.abs(P[i].x - midPoint.x) < d)
-                strip.push(P[i])
+            if (Math.abs(P[i].x - midPoint.x) < d){
+                borderPoints.push(P[i])
+            }
         }
-        // var result = d
-        console.log('oowowo')
-        return Math.min(d,CheckClosest(strip,d))
+
+        var result = Math.min(d,CheckBorderPoints(borderPoints,d))
+        console.log('the result is ' + result)
+        return result
     }
 
-function CheckClosest(points,minDist){
-    if(points.length < 2)
-        return minDist
+function CheckBorderPoints(points,minDist){
+    if(points.length <2)
+        return minDist;
     InsertSort(points,false)
     console.log('the size ' + points.length)
+    var list = []
     var min = minDist
     for (var i = 0; i < points.length-1; i++) {
-        for (var j = i+1; j < points.length && points[j].y-points[i].y < min; j++) {
-            if (dist(points[i],points[j]) < min)
-                thing = []
-                thing.push(points[i])
-                thing.push(points[j])
+        for (var j = i+1; j < points.length && (points[j].y-points[i].y < min); j++) {
+            if (dist(points[i],points[j]) < min){
+                list = []
+                list.push(points[i])
+                list.push(points[j])
                 min = dist(points[i], points[j]);
-                console.log('the min ' + min)
+                console.log('min '+ min)
+            }
            }
+    }
+    if(list.length ==2){
+        pointsToCheck.push(list[0])
+        pointsToCheck.push(list[1])
     }
     return min;
 }
@@ -189,12 +227,12 @@ function InsertSort(array, compareX = true){
 		greater than key, to one position ahead
         of their current position */
         if(compareX){
-		while (j >= 0 && array[j].x > key.x)
-		{
-			array[j + 1] = array[j];
-			j = j - 1;
+            while (j >= 0 && array[j].x > key.x)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
         }
-    }
         else{
             while (j >= 0 && array[j].y > key.y)
             {
@@ -206,3 +244,10 @@ function InsertSort(array, compareX = true){
 	}
     }
 }
+
+function clearCanvas(context, canvas) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    var w = canvas.width;
+    canvas.width = 1;
+    canvas.width = w;
+  }
